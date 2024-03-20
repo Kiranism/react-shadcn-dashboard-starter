4682d98046c11e6795053fb46d665216f2232949
Kiranism <iamkiran2831@gmail.com>
2024-03-20 19:57:22 +0530

    chore: inital setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true }
