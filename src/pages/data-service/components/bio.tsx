688c03e2411a7df928855db997bb31c5dd771f6c
darmawguna <komangagus642@gmail.com>
2024-05-02 08:14:46 +0800

    add new page

---
+++ src/pages/data-service/components/bio.tsx
@@ -0,0 +1,13 @@
+export default function Bio() {
+  return (
+    <div className="mb-4 mt-3 rounded-3xl bg-secondary xl:mt-0">
+      <h1 className="flex justify-start p-5 text-xl font-extrabold">Bio</h1>
+      <p className="px-5 pb-5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </div>
+  );
+}
